8fdf6fd7406278cf0321cd321e2dd75ba6f28b15
masssu116 <kakeru1999116@yahoo.co.jp>
2019-11-07 10:34:56 +0900

    ブロックの追加。サイズ変えてないですごめんなさい

---
+++ TORIKESU/revz.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class revz here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class revz extends Actor
+{
+    /**
+     * Act - do whatever the revz wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
