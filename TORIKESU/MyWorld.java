172d4e57069490429413c129c2fa62bee54009c7
kazuikazui <4rakune.24@gmail.com>
2019-12-19 09:43:57 +0900

    [add]

--- TORIKESU/MyWorld.java
+++ TORIKESU/MyWorld.java
@@ -1,5 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.*;
 /**
  * Write a description of class MyWorld here.
  * 
@@ -17,5 +19,10 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1); 
+        
+        if(Greenfoot.isKeyDown("m")){
+            JFrame frame = new JFrame();
+            String value = JOptionPane.showInputDialog(frame, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        }
     }
 }
