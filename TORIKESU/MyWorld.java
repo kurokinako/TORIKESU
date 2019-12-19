5bc04e59a8b6bf1586506f8115f657b84e142cb8
kurokinako <15105@hakodate.kosen-ac.jp>
2019-12-19 10:02:02 +0900

    コンフリクトを修正

--- TORIKESU/MyWorld.java
+++ TORIKESU/MyWorld.java
@@ -17,16 +17,14 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-<<<<<<< HEAD
-        super(600, 400, 1); 
-=======
+      
         super(1280, 720, 1); 
         
         if(Greenfoot.isKeyDown("m")){
             JFrame frame = new JFrame();
             String value = JOptionPane.showInputDialog(frame, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         }
->>>>>>> 172d4e57069490429413c129c2fa62bee54009c7
+
     }
     
     public void act()
